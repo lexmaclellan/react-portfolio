243b5a04c58fe7a20efa0330b7e4675959705397
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-15 01:09:35 -0300

    Removed bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
-import Hello from './components/Header'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Hello />
+      <Header />
     </>
   )
 }
