e420ac1e17e4a4bfb04c6bfbd373d141f0bdd9d3
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-15 03:20:22 -0300

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Navbar'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Project from './components/Project'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
