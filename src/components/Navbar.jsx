0d376dced557a1e0e9fc3db1e2809a0196428b6c
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-15 01:34:34 -0300

    Changed name of header component to navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar() {
+    const hello = "Hello World"
+
+    return (
+        <nav>
+            <div className="row">
+                <div className="col text-center">
+                    About
+                </div>
+                <div className="col text-center">
+                    Portfolio
+                </div>
+                <div className="col text-center">
+                    Contact
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
