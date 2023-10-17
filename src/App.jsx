ebb9970195cd457add09312e976e5e0e69750494
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-17 03:39:04 -0300

    Added page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Project from './components/Project'
 
 function App() {
   return (
-    <>
+    <main className='container-fluid'>
       <Header />
+      <article className='container-fluid'>
+        <Outlet />
+      </article>
       <Footer />
-    </>
+    </main>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import App from './App'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
+import Resume from './pages/Resume'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: '/Contact',
+        element: <Contact />
+      },
+      {
+        path: '/Resume',
+        element: <Resume />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
