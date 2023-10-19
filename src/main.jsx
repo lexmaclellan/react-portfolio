ea26fbcb3969532cc6920e5e53234e3a15601c28
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-18 22:04:05 -0300

    Added file extensions to imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import App from './App'
-import Home from './pages/Home'
-import Portfolio from './pages/Portfolio'
-import Contact from './pages/Contact'
-import Resume from './pages/Resume'
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import Contact from './pages/Contact.jsx'
+import Resume from './pages/Resume.jsx'
 
 const router = createBrowserRouter([
   {
