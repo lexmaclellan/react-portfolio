f2df72a3b9eaf6c848698304fb1b1cfe8939ab5d
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-18 22:08:55 -0300

    Updated file extensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Project from './components/Project'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
