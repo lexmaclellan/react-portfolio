a4f6d6de5fada0b1856f07b874252d3287e9db3b
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-18 21:50:32 -0300

    Revert "Styling for links, portfolio sample and email form"
    This reverts commit 7da604756172f862f99190aa6db49aa6faa2752e.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,21 @@ function Home() {
             <h1>Lex MacLellan <span className='pronouns'>(she/her, they/them)</span></h1>
             <h2>Web Developer</h2>
             <p>
-                My name is Lex and I'm a web developer based out of Atlantic Canada.
+                Nihil ea vitae recusandae ullam atque nemo. Ut eaque commodi doloribus
+                assumenda dicta quia aut praesentium. Et voluptatem rem facilis
+                necessitatibus veritatis doloremque quia et.
             </p>
             <p>
-                I am primarily a backend developer working with the MERN stack
-                and RESTful APIs.
+                Illo est ullam culpa. Unde delectus sed alias perferendis
+                asperiores unde quo neque. Ex praesentium doloribus rerum
+                pariatur magni. Dolor ea sapiente unde. Ea veritatis cumque
+                sunt unde. Est provident quaerat omnis in voluptate provident.
             </p>
             <p>
-                In my free time, I enjoy creative writing and playing the guitar.
+                Doloribus voluptatum aperiam provident excepturi dolor aliquam.
+                Adipisci omnis vero earum quia corrupti sunt. Nostrum atque
+                rerum omnis quia autem. Assumenda voluptate vel praesentium.
+                Hic qui laudantium fugit.
             </p>
         </>
     )
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,7 @@
 function Resume() {
     return (
         <>
-        <h1>Resume</h1>
-        <p>Available <a href="https://docs.google.com/document/d/1fjqpnh-8MqFL9HKPCkVzj0YGS8cUB52wsmOqWqpPy3c/edit?usp=sharing">here</a>!</p>
+        Resume
         </>
     )
 }
