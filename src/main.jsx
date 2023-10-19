aa3118130a26ee7e213a9fdf50105abfb757557b
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-18 21:57:21 -0300

    Fixed routing

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,10 @@ const router = createBrowserRouter([
         element: <Home />
       },
       {
+        path: '/react-portfolio',
+        element: <Home />
+      },
+      {
         path: '/Portfolio',
         element: <Portfolio />
       },
