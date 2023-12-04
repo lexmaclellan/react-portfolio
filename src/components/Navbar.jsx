4d1b7f15383199ffa0c6208f15b1279c5fdda645
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-12-04 02:38:24 -0400

    resized nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ function Navbar() {
     return (
         <nav className="fixed-top text-center">
             <div className="row p-3">
-                <div className="col-sm-3">
+                <div className="col-sm-4">
                     <Link to='/'>About</Link>
                 </div>
-                <div className="col-sm-3">
+                <div className="col-sm-4">
                     <NavLink to='/Portfolio'>Portfolio</NavLink>
                 </div>
-                <div className="col-sm-3">
+                <div className="col-sm-4">
                     <NavLink to='/Contact'>Contact</NavLink>
                 </div>
             </div>
